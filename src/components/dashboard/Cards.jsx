f4f36df7db0d0c661de95f71549de7f3b4b1bdb7
Viswanath-allamraju <allamrajuviswanath@gmail.com>
2021-02-09 21:02:50 +0530

    updating frontend

---
+++ src/components/dashboard/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Cards extends React.Component {
+    constructor(){
+        super();
+    }
+    render (){
+        return <div className="cards">
+
+        </div>
+    }
+}
