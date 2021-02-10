9ce0749b049d38d7474825b1cdffdde32e0dfce7
Viswanath-allamraju <allamrajuviswanath@gmail.com>
2021-02-10 20:05:57 +0530

    lists updates for tasks buttons

--- frontend/src/components/UserTaskLists.jsx
+++ frontend/src/components/UserTaskLists.jsx
@@ -12,6 +12,58 @@ export default class UserTaskLists extends React.Component {
             { field: 'id', headerName: 'ID', width: 70 },
             { field: 'taskname', headerName: 'Name', width: 130 },
             { field: 'taskstatus', headerName: 'Status', width: 130 },
+            {
+                field: "update",
+                headerName: "Update Status",
+                sortable: false,
+                width: 150,
+                disableClickEventBubbling: true,
+                renderCell: (params) => {
+                  const onClick = () => {
+                    const api = params.api;
+                    console.log(api);
+                    const fields = api
+                      .getAllColumns()
+                      .map((c) => c.field)
+                      .filter((c) => c !== "__check__" && !!c);
+                    const thisRow = {};
+            
+                    fields.forEach((f) => {
+                      thisRow[f] = params.getValue(f);
+                    });
+                    console.log(thisRow);
+                    //return alert(JSON.stringify(thisRow, null, 4));
+                  };
+            
+                  return <button onClick={onClick}>Modify State</button>;
+                }
+              },
+              {
+                field: "delete",
+                headerName: "Delete Task",
+                sortable: false,
+                width: 150,
+                disableClickEventBubbling: true,
+                renderCell: (params) => {
+                  const deleteTask = () => {
+                    const api = params.api;
+                    console.log(api);
+                    const fields = api
+                      .getAllColumns()
+                      .map((c) => c.field)
+                      .filter((c) => c !== "__check__" && !!c);
+                    const thisRow = {};
+            
+                    fields.forEach((f) => {
+                      thisRow[f] = params.getValue(f);
+                    });
+                    console.log(thisRow);
+                    //return alert(JSON.stringify(thisRow, null, 4));
+                  };
+            
+                  return <button onClick={deleteTask}>Delete</button>;
+                }
+              },
           ];
         const rows = [];
         userTask.map((tasks)=>{
